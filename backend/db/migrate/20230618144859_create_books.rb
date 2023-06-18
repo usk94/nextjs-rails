ef9df4850d064da76c528c7db70a5b4f47bf2021
Yusaku <hitoyasablue@gmail.com>
2023-06-19 01:17:06 +0900

    create table and migrate

---
+++ backend/db/migrate/20230618144859_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :published_at
+      t.string :description
+      t.integer :page_count
+      t.string :author
+      t.string :image
+      t.timestamps
+    end
+  end
+end
