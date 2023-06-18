4816ab6b7a3b62f24c39f7e3762d34fdfa847d64
Yusaku <hitoyasablue@gmail.com>
2023-06-19 01:17:32 +0900

    create books controller

---
+++ backend/app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+    b = Book.all
+    p "hogehoge", b
+    render json: { books: Book.all }
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: { book: book }
+  end
+
+  def create
+    book = Book.create!(book_params)
+    book.meanings.create!(meaning_params)
+  end
+
+  private
+    def book_params
+      params[:post].require(:book).permit(:body)
+    end
+end
