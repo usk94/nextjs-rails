9bfb5c6732ca34ef1bda293ca5b92471b1c91327
Yusaku <hitoyasablue@gmail.com>
2023-07-02 20:39:14 +0900

    remove unuse

--- frontend/app/_components/BookCard.tsx
+++ frontend/app/_components/BookCard.tsx
@@ -5,7 +5,7 @@ const BookCard = ({ book }: { book: Book }) => {
   return (
     <button className="bg-secondary-light flex rounded-xl w-72 h-48 m-5 items-center">
       <Image src={book.image || "/noImage.jpg"} alt={book.title} width={100} height={150} className="min-w-fit" />
-      <div className="flex flex-col truncate-multiline w-40">
+      <div className="flex flex-col w-40">
         <p>{book.title}</p>
         <p className="text-sm line-clamp-3">{book.description}</p>
         <p className="text-sm">著者: {book.author}</p>
