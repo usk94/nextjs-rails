3a5a0a541e1db079db5479f4eb73509cd8167348
Yusaku <hitoyasablue@gmail.com>
2023-07-15 20:33:33 +0900

    fix page style

--- frontend/app/upload/page.tsx
+++ frontend/app/upload/page.tsx
@@ -26,41 +26,43 @@ const Page = () => {
   }
 
   const handleClickSubmitButton = async () => {
-    console.log("here2!")
+    alert("here2!")
     // await trigger({ book: value })
   }
 
   return (
-    <div className="bg-neutral flex flex-col">
-      <div>
-        <div className="text-lg font-semibold">クーポンを追加する</div>
-        <span className="mt-2 text-sm font-light">
-          クーポンコードをお持ちの方は、以下のフォームより
-          <br />
-          クーポンコードを入力してください。
-        </span>
-        <div className="mt-4 flex">
-          <div className="grow-2 leading-none">
-            <input
-              type="text"
-              onChange={handleChange}
-              className="h-10 w-full border border-gray-lighter p-2 text-base font-light focus:border-primary focus:outline-none"
-              placeholder="例）winter2020"
-            />
+    <div className="bg-neutral w-screen h-screen">
+      <div className="flex flex-col">
+        <div>
+          <div className="text-lg font-semibold">クーポンを追加する</div>
+          <span className="mt-2 text-sm font-light">
+            クーポンコードをお持ちの方は、以下のフォームより
+            <br />
+            クーポンコードを入力してください。
+          </span>
+          <div className="mt-4 flex">
+            <div className="grow-2 leading-none">
+              <input
+                type="text"
+                onChange={handleChange}
+                className="h-10 w-full border border-gray-lighter p-2 text-base font-light focus:border-primary focus:outline-none"
+                placeholder="例）winter2020"
+              />
+            </div>
+            <button
+              type="button"
+              onClick={handleClickAddButton}
+              disabled={isMutating}
+              className="ml-3 rounded border border-gray px-4 py-2 text-base font-semibold leading-none"
+            >
+              追加
+            </button>
           </div>
-          <button
-            type="button"
-            onClick={handleClickAddButton}
-            disabled={isMutating}
-            className="ml-3 rounded border border-gray px-4 py-2 text-base font-semibold leading-none"
-          >
-            追加
-          </button>
         </div>
+        <button type="button" onClick={handleClickSubmitButton} className="ml-3 rounded border border-gray px-4 py-2 text-base font-semibold leading-none w-24">
+          submit!
+        </button>
       </div>
-      <button type="button" onClick={handleClickSubmitButton} className="ml-3 rounded border border-gray px-4 py-2 text-base font-semibold leading-none w-24">
-        submit!
-      </button>
     </div>
   )
 }
