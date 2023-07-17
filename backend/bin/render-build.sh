7f93bd10ce8866e58507dfbdbfb8e11ec5a980c4
Yusaku <hitoyasablue@gmail.com>
2023-07-18 06:26:20 +0900

    for seed

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -5,10 +5,12 @@ set -o errexit
 cd backend
 bundle install
 
-if [ "$(git rev-list --count HEAD)" -gt 1 ] && git diff --name-only HEAD HEAD~1 | grep -q "db/migrate"
-then
-  echo "Migration files have changed. Running bundle exec rake db:migrate..."
-  bundle exec rake db:migrate
-else
-  echo "No new migration files."
-fi
+bundle exec rake db:migrate
+bundle exec rake db:seed
+# if [ "$(git rev-list --count HEAD)" -gt 1 ] && git diff --name-only HEAD HEAD~1 | grep -q "db/migrate"
+# then
+#   echo "Migration files have changed. Running bundle exec rake db:migrate..."
+#   bundle exec rake db:migrate
+# else
+#   echo "No new migration files."
+# fi
