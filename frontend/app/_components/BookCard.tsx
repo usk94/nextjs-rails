e29527d6c5458d5f5ef4ef95eaee0a2bfb5146ff
Yusaku <hitoyasablue@gmail.com>
2023-07-20 08:03:09 +0900

    ml

--- frontend/app/_components/BookCard.tsx
+++ frontend/app/_components/BookCard.tsx
@@ -7,7 +7,7 @@ const BookCard = ({ book }: { book: Book }) => {
     <Link href={`book/${book.id}`} className="bg-secondary-light flex rounded-xl w-72 h-48 m-5 p-4 items-center">
       {/* TODO: next/imageにする */}
       <img src={book.image || "/noImage.jpg"} alt={book.title} width={100} height={150} className="min-w-2/5" />
-      <div className="flex flex-col max-w-40">
+      <div className="ml-2 flex flex-col max-w-40">
         <p className="text-sm">{book.title}</p>
         <p className="text-sm">著者: {book.author}</p>
       </div>
