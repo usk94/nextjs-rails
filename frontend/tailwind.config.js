0bf97b81efb48c39b56b394e6cc6acc7fdfee378
Yusaku <hitoyasablue@gmail.com>
2023-07-30 13:45:36 +0900

    import font instead of store in file system

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -30,9 +30,6 @@ module.exports = {
       "3/5": "60%",
     },
     extend: {
-      fontFamily: {
-        zen: ["zen", "sans-serif"],
-      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
