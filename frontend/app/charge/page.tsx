097ee6bd1d9bff9790d5dc30f609140418ee4360
Yusaku <hitoyasablue@gmail.com>
2023-08-02 00:27:45 +0900

    add animated button by framer motion

--- frontend/app/charge/page.tsx
+++ frontend/app/charge/page.tsx
@@ -1,5 +1,30 @@
+"use client"
+
+import { motion } from "framer-motion"
+
 const Page = () => {
-  return <p>bkb! hiiiya!</p>
+  const handleTap = (event: MouseEvent | TouchEvent | PointerEvent) => {
+    console.log("タップしたよ！")
+  }
+
+  return (
+    <div className="w-screen h-screen">
+      <div className="flex justify-center items-center">
+        <motion.button
+          onTap={handleTap}
+          className="text-7xl"
+          whileHover={{
+            scale: 1.2,
+            // transition: { duration: 0.2 },
+            transition: { type: "spring", stiffness: 400, damping: 17 },
+          }}
+          whileTap={{ scale: 0.9 }}
+        >
+          💎
+        </motion.button>
+      </div>
+    </div>
+  )
 }
 
 export default Page
