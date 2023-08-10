b6dd3e3e6d2e70551d814bc346c663b0f7b17642
Yusaku <hitoyasablue@gmail.com>
2023-08-10 16:06:52 +0900

    minor fix

--- backend/bin/render-build.sh
+++ backend/bin/render-build.sh
@@ -6,7 +6,8 @@ cd backend
 bundle install
 
 bundle exec rake db:migrate
-bundle exec rake db:seed
+# bundle exec rake db:seed
+
 # if [ "$(git rev-list --count HEAD)" -gt 1 ] && git diff --name-only HEAD HEAD~1 | grep -q "db/migrate"
 # then
 #   echo "Migration files have changed. Running bundle exec rake db:migrate..."
